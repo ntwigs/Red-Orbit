299f06b82a96798ff3e69831387177adc25db0e1
NorthernTwig <on222bx@student.lnu.se>
2016-01-12 15:34:06 +0100

    Memory fix

--- examination/client/source/js/memory/checkPair.js
+++ examination/client/source/js/memory/checkPair.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
- * Gamelogic (Is it pair? What if it isn't? Etc.)
+ * Gamelogic (Is it pair? What if it isn't? Can the user press? Etc.)
  */
 function checkPair() {
     var container = document.querySelectorAll(".card-container");
@@ -22,102 +22,115 @@ function checkPair() {
     var cardsInWindow = container[counter - 1].querySelectorAll(".card");
     var counterInWindow = container[counter - 1].parentElement.querySelector(".clickCounter");
 
-    function checkEnter() {
+    function outbreak(whichElement) {
+        newArr.push(whichElement.parentElement.className);
+        saveTarget.push(whichElement);
+    }
+
+    function onPair() {
+        saveTarget[0].classList.add("aPair");
+        saveTarget[1].classList.add("aPair");
+        saveTarget[0].setAttribute("tabindex", "0");
+        saveTarget[0].setAttribute("tabindex", "0");
+        clicks = 0;
+        pairCounter += 1;
+        if (pairCounter >= 8) {
+            winCheck.win(counterInWindow);
+        }
+    }
+
+    function notPair(theTheme) {
+
+        if (localStorage.theme !== "") {
+
+            // var lastTheme = localStorage.getItem("theme");
+            saveTarget[0].style.backgroundImage = "url('../image/" + theTheme + "/0.png')";
+            saveTarget[1].style.backgroundImage = "url('../image/" + theTheme + "/0.png')";
+        } else {
+            saveTarget[0].style.backgroundImage = "url('../image/plain/0.png')";
+            saveTarget[1].style.backgroundImage = "url('../image/plain/0.png')";
+        }
+
+        clicks = 0;
+    }
+
+    function hej(whichElement, currentTheme) {
+        if (targetArr[0] !== targetArr[1]) {
+            if (newArr.length < 1) {
+                outbreak(whichElement);
+            } else if (newArr.length < 2) {
+                if (targetArr[0] && targetArr[1]) {
+                    outbreak(whichElement);
+
+                }
+            } else if (newArr.length >= 2) {
+                newArr.length = 0;
+                saveTarget.length = 0;
+                outbreak(whichElement);
+            }
+
+            if (newArr[0] && newArr[1]) {
+                if (newArr[0] === newArr[1]) {
+                    setTimeout(onPair, 1000);
+                } else {
+                    setTimeout(function() {
+                        notPair(currentTheme);
+                    }, 1000);
+                }
+            }
+        }
+    }
+
+    function checkEnter(selection, event) {
         if (event.keyCode === 13) {
-            this.click();
+            selection.click();
         }
 
         event.preventDefault();
     }
 
-    function listener(event) {
+    function listener(whichElement) {
 
         if (clicks < 2) {
-
+            
             clicks += 1;
 
             tries += 1;
 
-            var getWindow = this.parentElement.parentElement.parentElement.parentElement;
+            var getWindow = whichElement.parentElement.parentElement.parentElement.parentElement;
             var currentTheme = getWindow.getAttribute("data-theme");
 
-            // if (localStorage.theme !== "") {
-            this.style.backgroundImage = "url('../image/" + currentTheme + "/" + this.parentElement.className + ".png')";
-
-            // } else {
-            //   this.style.backgroundImage = "url('../image/plain/0.png')";
-            // }
-
-            //Här ska man kunna ändra vilken bilden ska vara.
+            whichElement.style.backgroundImage = "url('../image/" + currentTheme + "/" + whichElement.parentElement.className + ".png')";
 
             if (targetArr.length >= 2) {
                 targetArr.length = 0;
             }
 
             if (targetArr.length < 2) {
-                targetArr.push(this);
+                targetArr.push(whichElement);
             }
 
             if (targetArr[0] === targetArr[1]) {
                 targetArr = targetArr.splice(0, 1);
                 clicks = clicks -= 1;
+                tries = tries -= 1;
                 pairCounter = pairCounter -= 1;
             }
 
             counterInWindow.textContent = tries;
 
-            if (targetArr[0] !== targetArr[1]) {
-                if (newArr.length < 1) {
-                    newArr.push(this.parentElement.className);
-                    saveTarget.push(this);
-                } else if (newArr.length < 2) {
-                    if (targetArr[0] && targetArr[1]) {
-                        newArr.push(this.parentElement.className);
-                        saveTarget.push(this);
-                    }
-                } else if (newArr.length >= 2) {
-                    newArr.length = 0;
-                    saveTarget.length = 0;
-                    newArr.push(this.parentElement.className);
-                    saveTarget.push(this);
-                }
+            hej(whichElement, currentTheme);
 
-                if (newArr[0] && newArr[1]) {
-                    if (newArr[0] === newArr[1]) {
-                        setTimeout(function() {
-                            saveTarget[0].classList.add("aPair");
-                            saveTarget[1].classList.add("aPair");
-                            saveTarget[0].setAttribute("tabindex", "0");
-                            saveTarget[0].setAttribute("tabindex", "0");
-                            clicks = 0;
-                            pairCounter += 1;
-                            if (pairCounter >= 8) {
-                                winCheck.win(counterInWindow);
-                            }
-                        }, 1000);
-                    } else {
-                        setTimeout(function() {
-
-                            if (localStorage.theme !== "") {
-                                // var lastTheme = localStorage.getItem("theme");
-                                saveTarget[0].style.backgroundImage = "url('../image/" + currentTheme + "/0.png')";
-                                saveTarget[1].style.backgroundImage = "url('../image/" + currentTheme + "/0.png')";
-                            } else {
-                                saveTarget[0].style.backgroundImage = "url('../image/plain/0.png')";
-                                saveTarget[1].style.backgroundImage = "url('../image/plain/0.png')";
-                            }
-
-                            clicks = 0;
-                        }, 1000);
-                    }
-                }
-            }
         }
     }
 
     for (i = 0; i < cardsInWindow.length; i += 1) {
-        cardsInWindow[i].addEventListener("keypress", checkEnter);
-        cardsInWindow[i].addEventListener("click", listener);
+        cardsInWindow[i].addEventListener("keypress", function() {
+            checkEnter(this, event);
+        });
+        cardsInWindow[i].addEventListener("click", function() {
+            listener(this);
+        });
     }
 }
 
